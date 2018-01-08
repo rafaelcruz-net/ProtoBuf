a360701f9d92b49aafc5798db33b5793d46d87b8
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2018-01-08 19:32:42 -0200

    Initial Commit

---
+++ Protobuf/Protobuf/Object/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protobuf.Object
+{
+    [ProtoContract()]
+    public class Cliente
+    {
+        [ProtoMember(1)]
+        public string PrimeroNome { get; set; }
+
+        [ProtoMember(2)]
+        public string UltimoNome { get; set; }
+
+        [ProtoMember(3)]
+        public DateTime DataNascimento { get; set; }
+
+        [ProtoMember(4)]
+        public List<Endereco> Enderecos { get; set; }
+    }
+
+    [ProtoContract()]
+    public class Endereco
+    {
+        [ProtoMember(1)]
+        public string Logradouro { get; set; }
+
+        [ProtoMember(2)]
+        public string Complemento { get; set; }
+
+        [ProtoMember(3)]
+        public string Bairro { get; set; }
+
+        [ProtoMember(4)]
+        public string Cidade { get; set; }
+
+        [ProtoMember(5)]
+        public string Pais { get; set; }
+
+    }
+}
---
+++ Protobuf/Protobuf/Program.cs
@@ -0,0 +1,72 @@
+﻿using Protobuf.Object;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protobuf
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                var obj = CreateCliente(i);
+                Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            }
+
+            stopWatch.Stop();
+
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+
+            Console.WriteLine("RunTime Json " + elapsedTime);
+
+            var stopProto = Stopwatch.StartNew();
+
+            for (int i = 0; i < 5000000; i++)
+            {
+                var obj = CreateCliente(i);
+                Protobuf.Serializer.ProtoSerialize.Serialize<Cliente>(obj);
+            }
+
+            stopProto.Stop();
+
+            TimeSpan tsProto = stopProto.Elapsed;
+
+            string elapsedTimeProto = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", tsProto.Hours, tsProto.Minutes, tsProto.Seconds, tsProto.Milliseconds / 10);
+
+            Console.WriteLine("RunTime Proto " + elapsedTimeProto);
+
+            Console.Read();
+        }
+
+        private static Cliente CreateCliente(int i)
+        {
+            return new Object.Cliente()
+            {
+                DataNascimento = DateTime.Now,
+                PrimeroNome = $"Cliente{i}",
+                UltimoNome = $"UltimoNome{i}",
+                Enderecos = new List<Endereco>()
+                {
+                    new Object.Endereco()
+                    {
+                        Bairro = $"Bairro{i}",
+                        Cidade = $"Cidade{i}",
+                        Complemento = $"Complemento{i}",
+                        Logradouro = $"Logradouro{i}",
+                        Pais = $"Pais{i}"
+                    }
+                }
+
+            }; 
+        }
+    }
+}
